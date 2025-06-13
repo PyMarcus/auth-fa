f0952922a2db13c42c9feb2adf1baef2ec868eda
PyMarcus <marcus-v@outlook.com>
2025-06-13 18:41:17 -0300

    feat: created models

--- models/user_model.py
+++ models/user_model.py
@@ -1,3 +1,13 @@
-class UserModel:
-    email: str
-    password: str
+from sqlalchemy import Column, Integer, String, Boolean
+
+from core.configs import settings
+
+
+class UserModel(settings.DBBaseModel):
+    __tablename__: str = "users"
+
+    id: int = Column(Integer, primary_key=True, autoincrement=True)
+    name: str = Column(String(256), nullable=False)
+    email: str = Column(String(256), index=True, nullable=False, unique=True)
+    password: str = Column(String(500), nullable=False)
+    admin: bool = Column(Boolean, default=False)
