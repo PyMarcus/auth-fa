810758f4244fe6d7be76f5c4e6ac73672f927fa0
PyMarcus <marcus-v@outlook.com>
2025-06-13 17:27:53 -0300

    create jwt function

--- core/auth.py
+++ core/auth.py
@@ -23,3 +23,31 @@ async def auth(email: EmailStr, password: str, db: AsyncSession) -> Optional[Use
 
         if not user:
             return None
+
+        if not verify_password(password, user.password):
+            return None
+
+        return user
+
+
+
+def create_access_token(sub: str) -> str:
+    return _create_token(
+        token_type="access_token",
+        time_to_live=timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES),
+        sub=sub
+    )
+
+
+def _create_token(token_type: str, time_to_live: timedelta, sub: str) -> str:
+    sp = timezone("AMERICA/Sao_Paulo")
+    expire = datetime.now(tz=sp) + time_to_live
+
+    payload = {
+        "type": token_type,
+        "exp": expire,
+        "iat": datetime.now(tz=sp),
+        "sub": str(sub)
+    }
+
+    return jwt.encode(payload, settings.JWT_SECRET, algorithm=settings.ALGORITH)
